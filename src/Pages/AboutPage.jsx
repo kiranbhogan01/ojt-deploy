7a40eee8730c06255d39e1aa65d1ceead50f8ff9
kiranbhogan01 <bhogank172@gmail.com>
2025-06-12 14:21:00 +0530

    commit4

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Header from "../components/Header/Header";
+
 import { Link } from "react-router-dom";
 import './AboutPage.css';
 import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
 
 const About = () => {
     return(
