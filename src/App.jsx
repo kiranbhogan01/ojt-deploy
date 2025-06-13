859d4f63c17609474f687477cc2993a865f2c397
kiranbhogan01 <bhogank172@gmail.com>
2025-06-13 11:45:08 +0530

    demo1 commit

--- src/App.jsx
+++ src/App.jsx
@@ -43,13 +43,28 @@ import Contact from "./Pages/ContactPage";
 import Admission from "./Pages/AdmissionPage";
 import NotFoundPage from "./Pages/NotFoundPage";
 import Chatbot from "./components/chatbotcomponent/Chatbot";
-
-
+import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup";
+import { useState } from "react";
 
 
 const App = () => {
+    const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
     return(
+      <>
         <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Kiran Raju Bhogan"
+          studentPhotoUrl="/Images/My.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
+      <div>
           <Router>
              <Routes>
                <Route path="/" element={<Home/>} />
@@ -63,9 +78,13 @@ const App = () => {
              <Chatbot />
           </Router>
           <Chatbot />
-        </div>
+      </div>
+        </>
         
     )
 }
 
 export default App;
+
+
+//https://ojt-deploy-git-main-kiran-bhogans-projects.vercel.app
